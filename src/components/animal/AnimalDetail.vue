6f1b2b20f7593a24879bb2d99e34d08bdce87090
Lévy Marques <prakkmak@gmail.com>
2020-12-26 17:17:03 +0100

    Factorisation des vues
    Mise au propre

--- src/components/animal/AnimalDetail.vue
+++ src/components/animal/AnimalDetail.vue
@@ -1,25 +1,30 @@
 <template>
-    <div class="columns">
-        <div class="column is-1">
-            <img src="https://dummyimage.com/320x200/000/fff"/>
-        </div>
-        <div class="column is-2">
-            <p v-if="!i">{{ data.name }}</p>
-        </div>
-        <div class="column has-text-left">
-            <p v-for="(datum, index) in data" :key="index"><b>{{ index }}</b> : {{ datum }}</p>
+    <div class="column">
+        <div v-on:click="() => extended = !extended" class="column is-clickable has-background-primary">{{ data.name }} </div>
+        <div v-if="extended" class="column">
+           <div class="columns">
+               <div class="column">
+                    <div v-for="(datum, index) in data" :key="index" class="columns" >
+                        <div v-if="datum" class="column is-2 has-background-info has-text-left"><p>{{ index }}</p></div>
+                        <div v-if="datum" class="column has-text-left" >{{ datum }}</div>
+                    </div>
+                </div>
+                <div class="column is-4">
+                    <figure class="image">
+                        <img src="https://dummyimage.com/320x200/000/fff"/>
+                    </figure>
+                </div>
+           </div> 
         </div>
     </div>
 </template>
 <script>
 export default {
     props: ['data'],
+    data() {
+        return {
+            extended: false
+        }
+    }
 }
 </script>
-<style lang="css">
-    img {
-        max-width:100%;
-        height:auto;
-        max-height:100%;
-    }
-</style>
