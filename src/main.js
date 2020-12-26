37aaa36b028ca6db139298a76d048ec134a66e57
Lévy Marques <prakkmak@gmail.com>
2020-12-26 09:18:06 +0100

    Ajout de i18n pour les locales
    https://vue-i18n-next.intlify.dev/essentials

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 
 require('./assets/main.scss');
 
-createApp(App).mount('#app')
+const i18n = createI18n({
+    // shomething vue-i18n options here ...
+  })
+  
+const app = createApp(App)
+
+app.use(i18n)
+
+app.mount('#app')
