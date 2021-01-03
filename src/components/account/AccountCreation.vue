21d2a01130ea5d4ecb416aa4c07a04a440396103
Lévy Marques <prakkmak@gmail.com>
2021-01-03 15:30:49 +0100

    Page de création et bcrypt
    Ajout de bcryptjs
    Création d'une page de création de compte de base

---
+++ src/components/account/AccountCreation.vue
@@ -0,0 +1,82 @@
+<template>
+  <div v-if="errors.length">
+    <b>Please correct the following error(s):</b>
+    <ul>
+      <li v-for="error in errors" :key="error">{{ error }}</li>
+    </ul>
+  </div>
+  <div class="field">
+    <p class="control has-icons-left has-icons-right">
+      <input class="input" type="email" placeholder="Email" v-model="mail">
+      <span class="icon is-small is-left">
+      <i class="fas fa-envelope"></i>
+    </span>
+      <span class="icon is-small is-right">
+      <i class="fas fa-check"></i>
+    </span>
+    </p>
+  </div>
+  <div class="field">
+    <p class="control has-icons-left">
+      <input class="input" type="password" placeholder="Password" v-model="password">
+      <span class="icon is-small is-left">
+      <i class="fas fa-lock"></i>
+    </span>
+    </p>
+  </div>
+  <div class="field">
+    <p class="control">
+      <button class="button is-success" @click="createAccount">
+        Login
+      </button>
+    </p>
+  </div>
+</template>
+
+<script>
+import bcryptjs from 'bcryptjs'
+
+export default {
+  name: "AccountCreation",
+  data(){
+    return{
+      mail: "",
+      password: "",
+      errors: [],
+    }
+  },
+  methods: {
+    checkForm(){
+      this.errors = []
+      if(this.mail.length < 5 || !this.mail.includes('@')){
+        this.errors.push(this.$t('account.errorMail'));
+      }
+      if(this.password.length < 8){
+        this.errors.push(this.$t('account.errorPassword'));
+      }
+      return this.errors.length === 0
+    },
+    async createAccount(){
+      if(!this.checkForm()) return;
+      try{
+        await fetch('http://localhost:8090/accounts',
+            {
+              method: "POST",
+              headers: {
+                'content-type': 'application/json'
+              },
+              body: {
+                mail: this.mail,
+                password: this.hashPassword(this.password)},
+            });
+      }
+      catch(err){
+        console.log(err);
+      }
+    },
+    async hashPassword(password){
+      return await bcryptjs.hash(password, 10);
+    }
+  }
+}
+</script>
