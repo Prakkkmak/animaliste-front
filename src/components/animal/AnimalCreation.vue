02a276f57177ade61f625c1381edb3061eb55793
Bastien L <b.lamour03@gmail.com>
2021-01-12 20:47:16 +0100

    Refacto eslint et prettier
    Refacto pour permettre la compilation en respectant les règles de prettier
    Modification du fichier conf .eslintrc pour la syntaxe des nom des vues.

--- src/components/animal/AnimalCreation.vue
+++ src/components/animal/AnimalCreation.vue
@@ -1,44 +1,53 @@
 <template>
-    <div class="columns has-background-light">
-        <div class="column is-1">
-            <button class="button is-primary" @click="isPopUpOpen = !isPopUpOpen">Nouvel animal</button>
-        </div>
+  <div class="columns has-background-light">
+    <div class="column is-1">
+      <button class="button is-primary" @click="isPopUpOpen = !isPopUpOpen">
+        Nouvel animal
+      </button>
     </div>
+  </div>
 
-    <div class="modal is-active" v-show="isPopUpOpen">
-        <div class="modal-background"></div>
-        <div class="modal-card">
-            <header class="modal-card-head">
-                <p class="modal-card-title">Ajout d'un animal</p>
-                <button class="delete" aria-label="delete" @click="isPopUpOpen = !isPopUpOpen"></button>
-            </header>
-            <section class="modal-card-body">
-                <AnimalCreationDetail @exit="validateCreationAndReload"/>
-            </section>
-            <footer class="modal-card-foot">
-                <button class="button" @click="isPopUpOpen = !isPopUpOpen">Annuler</button>
-            </footer>
-        </div>
+  <div class="modal is-active" v-show="isPopUpOpen">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Ajout d'un animal</p>
+        <button
+          class="delete"
+          aria-label="delete"
+          @click="isPopUpOpen = !isPopUpOpen"
+        ></button>
+      </header>
+      <section class="modal-card-body">
+        <AnimalCreationDetail @exit="validateCreationAndReload" />
+      </section>
+      <footer class="modal-card-foot">
+        <button class="button" @click="isPopUpOpen = !isPopUpOpen">
+          Annuler
+        </button>
+      </footer>
     </div>
+  </div>
 </template>
 
 <script>
-import AnimalCreationDetail from './AnimalCreationDetail.vue'
+import AnimalCreationDetail from './AnimalCreationDetail.vue';
+
 export default {
-    emits: ['animalCreated'],
-    components: {
-        AnimalCreationDetail
+  emits: ['animalCreated'],
+  components: {
+    AnimalCreationDetail,
+  },
+  data() {
+    return {
+      isPopUpOpen: false,
+    };
+  },
+  methods: {
+    validateCreationAndReload() {
+      this.isPopUpOpen = !this.isPopUpOpen;
+      this.$emit('animalCreated');
     },
-    data(){
-        return{
-            isPopUpOpen: false
-        }
-    },
-    methods: {
-        validateCreationAndReload(){
-            this.isPopUpOpen = !this.isPopUpOpen;
-            this.$emit('animalCreated');
-        }
-    }
-}
+  },
+};
 </script>
--- src/components/animal/AnimalCreationDetail.vue
+++ src/components/animal/AnimalCreationDetail.vue
@@ -1,70 +1,79 @@
 <template>
   <div class="columns">
     <div class="column is-2">
-        <label class="label">{{ $t('animalDetail.name') }}</label>
-        <input class="input is-primary" v-model="animal.name" type="text" placeholder="Chachounet"/>
+      <label class="label">{{ $t('animalDetail.name') }}</label>
+      <input
+        class="input is-primary"
+        v-model="animal.name"
+        type="text"
+        placeholder="Chachounet"
+      />
     </div>
     <div class="column is-2">
-        <label class="label">{{ $t('animalDetail.specie') }}</label>
-        <input class="input is-primary" v-model="animal.specie" type="text" placeholder="Chat"/>
+      <label class="label">{{ $t('animalDetail.specie') }}</label>
+      <input
+        class="input is-primary"
+        v-model="animal.specie"
+        type="text"
+        placeholder="Chat"
+      />
     </div>
     <div class="column is-3">
-        <label class="label">{{ $t('animalDetail.sex') }}</label>
-        <div class="select is-primary">
-            <select v-model="animal.sex">
-                <option
-                    v-for="option in sexes"
-                    :value="option === 'Mâle'"
-                    :key="option"
-                >{{ option }}</option>
-            </select>
-        </div>
+      <label class="label">{{ $t('animalDetail.sex') }}</label>
+      <div class="select is-primary">
+        <select v-model="animal.sex">
+          <option
+            v-for="option in sexes"
+            :value="option === 'Mâle'"
+            :key="option"
+          >
+            {{ option }}
+          </option>
+        </select>
+      </div>
     </div>
     <div class="column is-4">
-        <br/>
-        <button class="button is-success" @click="createAnimal">{{ $t('save') }}</button>
+      <br />
+      <button class="button is-success" @click="createAnimal">
+        {{ $t('save') }}
+      </button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            sexes: [
-                "Mâle",
-                "Femelle"
-            ],
-            animal: this.emptyAnimal()
-        }
+  data() {
+    return {
+      sexes: ['Mâle', 'Femelle'],
+      animal: this.emptyAnimal(),
+    };
+  },
+  emits: ['exit'],
+  methods: {
+    async createAnimal() {
+      const newAnimal = JSON.stringify(this.animal);
+      try {
+        await fetch(process.env.VUE_APP_BASE_URL + '/animals', {
+          method: 'POST',
+          body: newAnimal,
+          headers: {
+            'content-type': 'application/json',
+          },
+        });
+      } catch (err) {
+        console.log(err);
+      }
+      this.animal = this.emptyAnimal();
+      this.$emit('exit');
     },
-    emits: ['exit'],
-    methods: {
-        async createAnimal(){
-            let newAnimal = JSON.stringify(this.animal)
-            try{
-                await fetch(process.env.VUE_APP_BASE_URL + '/animals',
-                {
-                    method: "POST",
-                    body: newAnimal,
-                    headers: {
-                        'content-type': 'application/json'
-                    }
-                })
-            }
-            catch(err){
-                console.log(err);
-            }
-            this.animal = this.emptyAnimal();
-            this.$emit('exit');
-        },
-        emptyAnimal(){
-          return {
-            name: '',
-            specie: '',
-            sex: false
-          }
-        }
-    }
-}
+    emptyAnimal() {
+      return {
+        name: '',
+        specie: '',
+        sex: false,
+      };
+    },
+  },
+};
 </script>
