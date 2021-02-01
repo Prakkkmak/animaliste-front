972d87e67684ad128df046480e34e2db60cd50f1
Lévy Marques <prakkmak@gmail.com>
2021-02-01 13:44:49 +0100

    fix warnings

--- src/api/httpClient.ts
+++ src/api/httpClient.ts
@@ -24,7 +24,7 @@ httpClient.interceptors.request.use(
 );
 
 httpClient.interceptors.response.use(
-  function (response) {
+  (response) => {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return response;
