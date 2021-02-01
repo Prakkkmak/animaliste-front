1ba48d60c833bb043f604b138694d0e5cbaf00a1
Prakkmak <prakkmak@gmail.com>
2021-02-01 09:28:53 +0100

    Test AnimalField

---
+++ src/components/animal/detail/AnimalField.spec.ts
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/vue";
+import AnimalField from "./AnimalField.vue";
+
+const fieldData = {
+  key: "field_name",
+  value: "field_value",
+};
+
+beforeEach(() => {
+  render(AnimalField, {
+    props: {
+      fieldData,
+    },
+  });
+});
+
+test("should show fieldData", () => {
+  screen.getByText(/field_name/);
+  screen.getByText(/field_value/);
+});
