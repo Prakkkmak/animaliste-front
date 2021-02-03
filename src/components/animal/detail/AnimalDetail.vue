77e43be89792593e199e04b7037c1df99bb6f8ce
Bastien L <b.lamour03@gmail.com>
2021-02-03 16:00:32 +0100

    Animal detail refacto ts

--- src/components/animal/detail/AnimalDetail.vue
+++ src/components/animal/detail/AnimalDetail.vue
@@ -12,68 +12,68 @@
           <AnimalDatum
             :fieldData="getFieldData('name')"
             :edit="modification"
-            @update-field="updateAnimal"
+            @fieldUpdated="updateAnimal"
           />
           <AnimalDatum
             :fieldData="getFieldData('sex')"
             :edit="modification"
             :displayComponent="animalFieldDisplaySex"
-            @update-field="updateAnimal"
+            @fieldUpdated="updateAnimal"
           />
           <AnimalDatum
             :fieldData="getFieldData('specie')"
             :edit="modification"
-            @update-field="updateAnimal"
+            @fieldUpdated="updateAnimal"
           />
           <AnimalDatum
             :fieldData="getFieldData('race')"
             :edit="modification"
-            @update-field="updateAnimal"
+            @fieldUpdated="updateAnimal"
           />
           <AnimalDatum
             :fieldData="getFieldData('description')"
             :edit="modification"
-            @update-field="updateAnimal"
+            @fieldUpdated="updateAnimal"
           />
           <AnimalDatum
             :fieldData="getFieldData('likes')"
             :edit="modification"
-            @update-field="updateAnimal"
+            @fieldUpdated="updateAnimal"
           />
           <AnimalDatum
             :fieldData="getFieldData('dislikes')"
             :edit="modification"
-            @update-field="updateAnimal"
+            @fieldUpdated="updateAnimal"
           />
           <AnimalDatum
             :fieldData="getFieldData('vaccines')"
             :edit="modification"
-            @update-field="updateAnimal"
+            @fieldUpdated="updateAnimal"
           />
           <AnimalDatum
             :fieldData="getFieldData('nutrition')"
             :edit="modification"
-            @update-field="updateAnimal"
+            @fieldUpdated="updateAnimal"
           />
           <AnimalDatum
             :fieldData="getFieldData('origin')"
             :edit="modification"
-            @update-field="updateAnimal"
+            @fieldUpdated="updateAnimal"
           />
           <AnimalDatum
             :fieldData="getFieldData('chip')"
             :edit="modification"
-            @update-field="updateAnimal"
+            @fieldUpdated="updateAnimal"
           />
           <AnimalDatum
             :fieldData="getFieldData('registerDate')"
             :edit="modification"
-            @update-field="updateAnimal"
+            @fieldUpdated="updateAnimal"
           />
           <AnimalDatum
             :fieldData="getFieldData('id')"
             :edit="false"
-            @update-field="updateAnimal"
+            @fieldUpdated="updateAnimal"
           />
         </div>
         <div class="column is-4">
@@ -105,73 +105,90 @@
     </div>
   </div>
 </template>
-<script>
+<script lang="ts">
 import animalApi from "@/api/animal.api";
 import AnimalFieldDisplaySex from "@/components/animal/form/AnimalFieldDisplaySex";
 import toaster from "@/utils/toaster";
 import AnimalDatum from "./AnimalField.vue";
+import {Vue} from "vue-class-component";
+import {Emit, Prop} from "vue-property-decorator";
 
-export default {
+@Option({
   components: {
-    AnimalDatum,
-  },
-  props: ["id"],
-  emits: ["delete-animal", "update-animal"],
-  data() {
-    return {
-      data: {},
-      extended: false,
-      loading: true,
-      modification: false,
-      animalFieldDisplaySex: AnimalFieldDisplaySex,
-    };
-  },
+    AnimalDatum
+  }
+});
+
+export default class AnimalDetail extends Vue{
+  @Prop(String)
+  private readonly id : String | undefined;
+
+  private data = Object.create(null);
+
+  private extended : Boolean = false;
+
+  private loading : Boolean = true;
+
+  private modification : Boolean = false;
+
+  private animalFieldDisplaySex = AnimalFieldDisplaySex;
+
   async mounted() {
     await this.loadAnimal();
-  },
-  methods: {
-    getFieldData(key) {
-      return { key, value: this.data[key] };
-    },
-    async loadAnimal() {
-      this.loading = true;
-      try {
-        const res = await animalApi.getAnimalById(this.id);
-        this.data = res.data;
-      } catch (err) {
-        toaster.error("toasts.error.unknownError");
-      } finally {
-        this.loading = false;
-        this.modification = false;
-      }
-    },
-    async deleteAnimal() {
-      try {
-        await animalApi.deleteAnimal(this.data.id);
-      } catch (err) {
-        toaster.error("toasts.error.unknownError");
-      } finally {
-        this.$emit("delete-animal", this.data.id);
-      }
-    },
-    updateAnimal(newData) {
-      Object.keys(newData).forEach((data) => {
-        this.data[data] = newData[data];
-      });
-      this.modification = true;
-      this.$emit("update_animal", this.data);
-    },
-    cancelModification() {
-      this.loadAnimal();
-    },
-    async saveAnimal() {
-      try {
-        await animalApi.saveAnimal(this.data.id, this.data);
-      } catch (err) {
-        toaster.error("toasts.error.unknownError");
-      }
+  }
+
+  getFieldData(key) {
+    return { key, value: this.data[key] };
+  }
+
+  async loadAnimal() {
+    this.loading = true;
+    try {
+      const res = await animalApi.getAnimalById(this.id);
+      this.data = res.data;
+    } catch (err) {
+      toaster.error("toasts.error.unknownError");
+    } finally {
+      this.loading = false;
       this.modification = false;
-    },
-  },
+    }
+  }
+
+  async deleteAnimal() {
+    try {
+      await animalApi.deleteAnimal(this.data.id);
+    } catch (err) {
+      toaster.error("toasts.error.unknownError");
+    } finally {
+      this.animalDeleted(this.data.id)
+    }
+  }
+
+  updateAnimal(newData) {
+    Object.keys(newData).forEach((data) => {
+      this.data[data] = newData[data];
+    });
+    this.modification = true;
+    this.animalUpdated();
+  }
+
+  cancelModification() {
+    this.loadAnimal();
+  }
+
+  async saveAnimal() {
+    try {
+      await animalApi.saveAnimal(this.data.id, this.data);
+    } catch (err) {
+      toaster.error("toasts.error.unknownError");
+    }
+    this.modification = false;
+  }
+
+  @Emit()
+  animalUpdated(){}
+
+  @Emit()
+  animalDeleted(id){}
 };
 </script>
